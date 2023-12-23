76bb50b6086af92e24a0baeaf6e66f219090d0c5
Libin Johny <libinjohny210@gmail..com>
2023-12-23 21:56:58 +0000

    Initial commit

---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+  state = {
+    todoState : 'Todo component'
+  }
+
+  render() {
+    return (
+      <div>Todo component</div>
+    )
+  }
+}
